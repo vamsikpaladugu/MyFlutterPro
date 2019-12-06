c70cadee63448c2381901e129fbfe889b66b4885
Vamsi <vamsik.paladugu@gmail.com>
2019-12-06 23:37:02 +0530

    Fluent v1.0

---
+++ fluent/lib/utils/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static final Color bColor = Color(0xFFFFDE03);
+  //static final Color bColor = Color(0xFF0336FF);
+
+  static final Color aColor = Color(0xFF0336FF);
+  //static final Color aColor = Color(0xFFFF0266);
+
+}
